2acfeec5a4cb811db29d069334f339dee0986bdb
noordseoostende <voropayev1@mail.ru>
2022-04-17 08:44:29 +0200

    Draw implement

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
 "use strict";
+function GetRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function GetRandomInt(min, max) {
+    return Math.floor(GetRandomFloat(min, max));
+}
+function FromPolar(v, theta) {
+    return [v * Math.cos(theta), v * Math.sin(theta)];
+}
+var MaxParticleSize = 3;
+var Particle = /** @class */ (function () {
+    function Particle(w, h) {
+        this.w = w;
+        this.h = h;
+        this.x = 0;
+        this.y = 0;
+        this.speed = 0;
+        this.theta = 0;
+        this.radius = 1.0;
+        this.ttl = 500;
+        this.lifetime = 500;
+        this.color = 'black';
+        this.x = GetRandomFloat(0, w);
+        this.y = GetRandomFloat(0, h);
+        this.speed = GetRandomFloat(0, 3.0);
+        this.theta = GetRandomFloat(0, 2 * Math.PI);
+        this.radius = GetRandomFloat(0.05, MaxParticleSize);
+        this.lifetime = this.ttl = GetRandomInt(25, 50);
+    }
+    Particle.prototype.Update = function () {
+        //imp this
+    };
+    Particle.prototype.Draw = function (ctx) {
+        //imp this
+    };
+    return Particle;
+}());
+var ParticleCount = 200;
 var Simulation = /** @class */ (function () {
     function Simulation(width, height) {
         this.width = width;
         this.height = height;
+        this.particles = [];
+        for (var i = 0; i < ParticleCount; i++) {
+            this.particles.push(new Particle(this.width, this.height));
+        }
     }
     Simulation.prototype.Update = function () {
         //imp this
     };
     Simulation.prototype.Draw = function (ctx) {
         //imp this
+        ctx.fillStyle = 'green';
+        ctx.fillRect(0, 0, this.width, this.height);
     };
     return Simulation;
 }());
--- app.ts
+++ app.ts
@@ -1,15 +1,62 @@
+function GetRandomFloat(min:number, max:number):number{
+  return Math.random() * (max-min) + min
+}
+function GetRandomInt(min:number, max:number):number{
+  return Math.floor(GetRandomFloat(min,max))
+}
+function FromPolar(v:number, theta:number){
+  return [v * Math.cos(theta), v * Math.sin(theta)]
+}
 interface ISimObject {
   Update():void
   Draw(ctx:CanvasRenderingContext2D):void
 }
+const MaxParticleSize = 3
+class Particle implements ISimObject{
+  x = 0; y = 0;
+  speed = 0; theta = 0
+  radius = 1.0
+  ttl = 500
+  lifetime = 500
+  color = 'black'
+  constructor(private w:number, private h:number){
+    this.x = GetRandomFloat(0, w)
+    this.y = GetRandomFloat(0, h)
+
+    this.speed = GetRandomFloat(0, 3.0)
+    this.theta = GetRandomFloat(0, 2 * Math.PI)
+
+    this.radius = GetRandomFloat(0.05, MaxParticleSize)
+    this.lifetime = this.ttl = GetRandomInt(25, 50)
+  }
+  Update(){
+    //imp this
+   }
+   Draw(ctx:CanvasRenderingContext2D){
+     //imp this
+     
+   }
+}
+const ParticleCount = 200
 
 class Simulation implements ISimObject{
-  constructor(private width:number, private height:number){}
+  
+  particles:Particle[] = []
+  constructor(private width:number, private height:number){
+    for (let i = 0; i < ParticleCount; i++) {
+      this.particles.push(
+        new Particle(this.width, this.height)
+      )
+      
+    }
+  }
   Update(){
    //imp this
   }
   Draw(ctx:CanvasRenderingContext2D){
     //imp this
+    ctx.fillStyle = 'green'
+    ctx.fillRect(0,0,this.width,this.height)
   }
 }
 
